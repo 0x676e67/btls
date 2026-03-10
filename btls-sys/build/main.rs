99bcc2ecf4fd3edac0dfdcc84ec67cbac5c3593c
0x676e67 <gngppz@gmail.com>
2026-03-10 17:23:29 +0800

    Reorder windows cross compile patch application

--- btls-sys/build/main.rs
+++ btls-sys/build/main.rs
@@ -473,11 +473,12 @@ fn ensure_patches_applied(config: &Config) -> io::Result<()> {
 
     println!("cargo:warning=applying patch to boringssl");
     apply_patch(config, "boringssl.patch")?;
-    println!("cargo:warning=applying windows cross compile patch to boringssl");
-    apply_patch(config, "boringssl-windows.patch")?;
 
     println!("cargo:warning=applying loongarch patch to boringssl");
     apply_patch(config, "boringssl-loongarch.patch")?;
+
+    println!("cargo:warning=applying windows cross compile patch to boringssl");
+    apply_patch(config, "boringssl-windows.patch")?;
 
     if config.features.underscore_wildcards {
         println!("cargo:warning=applying underscore wildcards patch to boringssl");
