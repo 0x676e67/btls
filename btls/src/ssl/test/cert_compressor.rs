770103ad8e49099be5b17ef4ad83925d525c3581
gngpp <gngppz@gmail.com>
2026-03-23 16:08:11 +0800

    refactor: support `&'static dyn trait` for `CertificateCompressor`

--- btls/src/ssl/test/cert_compressor.rs
+++ btls/src/ssl/test/cert_compressor.rs
@@ -1,10 +1,11 @@
-use std::io::Write as _;
+use std::io::{self, Read, Write as _};
 
 use super::server::Server;
 use crate::ssl::CertificateCompressor;
 use crate::x509::store::X509StoreBuilder;
 use crate::x509::X509;
 
+#[derive(Debug)]
 struct BrotliCompressor {
     q: u32,
     lgwin: u32,
@@ -17,28 +18,36 @@ impl Default for BrotliCompressor {
 }
 
 impl CertificateCompressor for BrotliCompressor {
-    const ALGORITHM: crate::ssl::CertificateCompressionAlgorithm =
-        crate::ssl::CertificateCompressionAlgorithm(1234);
-
-    const CAN_COMPRESS: bool = true;
-
-    const CAN_DECOMPRESS: bool = true;
-
-    fn compress<W>(&self, input: &[u8], output: &mut W) -> std::io::Result<()>
-    where
-        W: std::io::Write,
-    {
+    fn compress(&self, input: &[u8], output: &mut dyn std::io::Write) -> std::io::Result<()> {
         let mut writer = brotli::CompressorWriter::new(output, 1024, self.q, self.lgwin);
         writer.write_all(input)?;
         Ok(())
     }
 
-    fn decompress<W>(&self, input: &[u8], output: &mut W) -> std::io::Result<()>
-    where
-        W: std::io::Write,
-    {
-        brotli::BrotliDecompress(&mut std::io::Cursor::new(input), output)?;
+    fn decompress(&self, input: &[u8], output: &mut dyn std::io::Write) -> std::io::Result<()> {
+        let mut reader = brotli::Decompressor::new(input, 4096);
+        let mut buf = [0u8; 4096];
+        loop {
+            match reader.read(&mut buf[..]) {
+                Err(e) => {
+                    if let io::ErrorKind::Interrupted = e.kind() {
+                        continue;
+                    }
+                    return Err(e);
+                }
+                Ok(size) => {
+                    if size == 0 {
+                        break;
+                    }
+                    output.write_all(&buf[..size])?;
+                }
+            }
+        }
         Ok(())
+    }
+
+    fn algorithm(&self) -> crate::ssl::CertificateCompressionAlgorithm {
+        crate::ssl::CertificateCompressionAlgorithm::BROTLI
     }
 }
 
